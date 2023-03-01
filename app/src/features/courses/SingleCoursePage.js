553a74000ccdf06668b44eb6813dfc9312d1f70a
Gary <gary>
2023-03-01 18:31:25 -0500

    Merged COUR-71 and COUR-72

--- app/src/features/courses/SingleCoursePage.js
+++ app/src/features/courses/SingleCoursePage.js
@@ -16,54 +16,48 @@ export const SingleCoursePage = () => {
         error
     } = useGetCourseQuery(courseId)
 
+    // const ReviewList = () => {
+    //     return (
+    //         <div className="reviews">
+    //             {course.reviews.map(review =>(
+    //                 <div className="reviewBlock">{review}</div>
+    //             ))}
+    //         </div>
+    //     );
+    // }
+
     let content
 
     if (isLoading) {
         content = <Spinner text="Loading..." />
     } else if (isSuccess) {
         content = (
-            <article className="course-excerpt" key={course.id}>
-            <h3>{course.name}</h3>
-            <p className="description">{course.description}</p>
-            </article>
-    const ReviewList = () => {
-        return (
-            <div className="reviews">
-                {course.reviews.map(review =>(
-                    <div className="reviewBlock">{review}</div>
-                ))}
-            </div>
-        );
-    }
+            <Container className="singleCoursePage">
+                <h2 className="courseName">{course.name}</h2>
+                <h3>{course.instructor} </h3>
+                <p className="courseDescription">{course.description}</p>
+                <div>
+                    <p className="rating">{course.rating}</p>
+                    <p className="rateOutof">/ 5</p>
+                    <button className="rate-review-but">Rate and Review</button>
+                </div>
+                <div className="ratingBar">
+                    <ProgressBar max = "5" now={course.rating} />
+                    <p className="counts">(Counts of total ratings)</p>
+                </div>
+                <h3 className="reviews-text">Reviews</h3>
+                <hr/>
+                {/*<ReviewList/>*/}
 
-    if (!course) {
-        return (
-            <Container>
-                <h2>Course not found!</h2>
             </Container>
         )
     } else if (isError) {
         content = <div>{error.toString()}</div>
     }
 
-
     return (
-        <Container className="singleCoursePage">
-            <h2 className="courseName">{course.name}</h2>
-            <h3>{course.instructor} </h3>
-            <p className="courseDescription">{course.description}</p>
-            <div>
-                <p className="rating">{course.rating}</p>
-                <p className="rateOutof">/ 5</p>
-                <button className="rate-review-but">Rate and Review</button>
-            </div>
-            <div className="ratingBar">
-                <ProgressBar max = "5" now={course.rating} />
-                <p class="counts">(Counts of total ratings)</p>
-            </div>
-            <h3 className="reviews-text">Reviews</h3>
-            <hr/>
-            <ReviewList/>
+        <Container className="course-detail">
+            {content}
         </Container>
     )
 }
