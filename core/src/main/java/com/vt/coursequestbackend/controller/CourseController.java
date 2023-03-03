1faa1f75ac616821cad153ca49ada8cc8d31bd95
EugeneFengfyc <eugenefeng.fyc@gmail.com>
2023-03-03 22:53:25 +0800

    Create Course related APIs

--- core/src/main/java/com/vt/coursequestbackend/controller/CourseController.java
+++ core/src/main/java/com/vt/coursequestbackend/controller/CourseController.java
@@ -1,5 +1,6 @@
 package com.vt.coursequestbackend.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CourseController {
 
+    @ApiOperation("This service is used to get the list of all the courses available in the university")
     @GetMapping("/api/university/{universityid}/courses")
     public String getCourseList(@PathVariable String universityid) {
         return "getCourseList: " + universityid;
     }
 
-    @GetMapping(" /api/university/{universityid}/courses/{courseid}")
-    public String getCourseDetails(@PathVariable String courseid, String universityid) {
+    @ApiOperation("This service is used to get a particular coursedetails")
+    @GetMapping("/api/university/{universityid}/courses/{courseid}")
+    public String getCourseDetails(@PathVariable String courseid,  @PathVariable String universityid) {
         return "getCourseDetails: " + "courseid: " + courseid + "universityid: " + universityid;
     }
+
+    @ApiOperation("This service is used to get the list of all the degree types available in the university\n")
+    @GetMapping("/api/university/{universityid}/degreetypes")
+    public String getDegreeList(@PathVariable String universityid) {
+        return "getDegreeList: " + "universityid: " + universityid;
+    }
+
 
 
 }
