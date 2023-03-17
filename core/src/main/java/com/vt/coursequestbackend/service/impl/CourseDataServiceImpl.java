ad371441c99747e959e7be00c3c1c17cf7e0c74b
swati-lamba <lswati@vt.edu>
2023-03-17 19:11:25 -0400

    implementation class

---
+++ core/src/main/java/com/vt/coursequestbackend/service/impl/CourseDataServiceImpl.java
@@ -0,0 +1,29 @@
+package com.vt.coursequestbackend.service.impl;
+
+import java.util.List;
+
+import com.vt.coursequestbackend.entity.Course;
+import com.vt.coursequestbackend.entity.Degree;
+import com.vt.coursequestbackend.service.CourseDataService;
+
+public class CourseDataServiceImpl implements CourseDataService{
+
+	@Override
+	public List<Course> getCourseList(String universityid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Course getCourseDetails(String courseid, String universityId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Degree> getDegreeList(String universityid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
