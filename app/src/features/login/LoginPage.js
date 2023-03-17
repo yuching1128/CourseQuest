fff7947f5104c178e7b3a391b586004b5bba773e
yuching1128 <yuching1128@vt.edu>
2023-03-17 15:35:05 -0400

    update loginPage styling

--- app/src/features/login/LoginPage.js
+++ app/src/features/login/LoginPage.js
@@ -1,8 +1,10 @@
 import Container from "react-bootstrap/Container";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import {Image, Stack} from "react-bootstrap";
 import { useForm } from "react-hook-form";
+import{faAnglesRight} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
 export default function LoginPage() {
 
@@ -15,29 +17,34 @@ export default function LoginPage() {
 
     return (
         <Container className="LoginPage">
-            <Stack direction="horizontal" gap={3} className="LoginContent">
+            <div className="welcomeBlock">
+                <p className="welcomeText">Welcome to CourseQuest!</p>
+                <div className="boxOrange">
+                    <div className="boxGreen">
+                        <div className="boxRed">
+                            <p className="loginText">Login to Start Explore Your Courses <FontAwesomeIcon icon={faAnglesRight} /></p>
+                            <p className="loginTextSmall">Connect with fellow students and get the inside scoop on courses to find your perfect course match with CourseQuest</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-                <Image alt="Logo" src={require('../../images/login-page-image.png') }
-                       width="400px"
-                       className="d-inline-block align-top " />
-
-                <Form onSubmit={handleSubmit(onSubmit)} className="LoginForm">
-                    <h1>Login</h1>
+            <div className="LoginForm">
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                    <p className="loginFromTitle">Login</p>
                     <Form.Group className="mb-3" controlId="loginEmail">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control placeholder="Enter email" {...register("email", { required: 'Email is required' })} />
+                        <Form.Label className="login-info">Email</Form.Label>
+                        <Form.Control className="login-component" placeholder="Enter email" {...register("email", { required: 'Email is required' })} />
                         {errors.email && <p className="errorMessage">{errors.email?.message}</p>}
                     </Form.Group>
-
                     <Form.Group className="mb-3" controlId="loginPassword" >
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password"  {...register("password", { required: 'Password is required' })} />
+                        <Form.Label className="login-info">Password</Form.Label>
+                        <Form.Control className="login-component" type="password" placeholder="Password"  {...register("password", { required: 'Password is required' })} />
                         {errors.password && <p className="errorMessage">{errors.password?.message}</p>}
                     </Form.Group>
-
-                    <Button variant="primary" type="submit" className="LoginButton">Login</Button>
+                    <button variant="primary" type="submit" className="main-login-button">Login</button>
                 </Form>
-            </Stack>
+            </div>
         </Container>
     );
 }
