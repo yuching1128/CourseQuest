a312ca1263be30a69db2fd7fe6b0eb021ab90574
vraghavi <vannalaraghavi@gmail.com>
2023-03-26 17:05:59 -0400

    add star component

--- app/src/features/reviews/RateReviewPage.js
+++ app/src/features/reviews/RateReviewPage.js
@@ -1,21 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 import { useParams } from "react-router-dom";
-import { Stack, Form, Button, Row, Col } from "react-bootstrap"
+import { Stack, Form, Button, Row, Col } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import { useForm } from "react-hook-form";
-
+import StarRatings from 'react-star-ratings';
+import '../../css/rateReviewPage.css';
 
 export const RateReview = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const [rating, setRating] = useState(0);
     const { courseId } = useParams()
     const onSubmit = (data) => {
         // TODO SUBMIT DATA TO API FOR PROCESSING
         console.log(data)
     }
+    
     return (<Container className="reviews">
         <Container className="RateReviewPage">
             <Stack direction="horizontal" gap={3} className="RateReviewContent"></Stack>
             <Form onSubmit={handleSubmit(onSubmit)} className="RateReviewForm">
+                <div className="stars-rating"><h3>Rate the Course</h3>
+                <StarRatings 
+                    rating={rating}
+                    changeRating={(newRating)=>{
+                        setRating(newRating);
+                    }}
+                    starDimension="3em"
+                    starSpacing="2em"
+                    starRatedColor ='rgb(237, 139, 0)'/></div>
+                    <div>
                 <h3>Write a Review</h3>
                 <Form.Group as={Row} className="mb-3" controlId="taughtBy">
                     <Form.Label column sm={2}>Taught by</Form.Label>
@@ -37,6 +51,10 @@ export const RateReview = () => {
                         <option>Default select</option>
                     </Form.Select></Col>
                 </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="workload">
+                <Form.Control as="textarea" aria-label="With textarea" placeholder='Things that you want share.' />
+                </Form.Group></div>
 
                 <Button variant="primary" type="submit" className="submit">Submit</Button>
                 <Button variant="secondary" type="cancel" className="submit">Cancel</Button>
