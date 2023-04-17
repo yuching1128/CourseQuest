9cda181f347cf0a53a41ca4ef3aa47c4248226f8
EugeneFengfyc <eugenefeng.fyc@gmail.com>
2023-04-16 21:17:54 -0400

    Permit All Request for frontend debugging

--- core/src/main/java/com/vt/coursequest/config/SecurityConfig.java
+++ core/src/main/java/com/vt/coursequest/config/SecurityConfig.java
@@ -48,10 +48,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().permitAll()
-                .and()
-                .logout().permitAll();
+                .anyRequest().permitAll();
+//                .and()
+//                .formLogin().permitAll()
+//                .and()
+//                .logout().permitAll();
     }
 }
