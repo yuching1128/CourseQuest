519442e1f648da4e61285ba6ff3e76e4ff468ede
swati-lamba <lswati@vt.edu>
2023-04-23 09:34:06 -0400

    comment es model

---
+++ core/src/main/java/com/vt/coursequest/elasticsearch/model/CommentModel.java
@@ -0,0 +1,25 @@
+package com.vt.coursequest.elasticsearch.model;
+
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Dynamic;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
+
+import lombok.Data;
+
+@Document(indexName = "comments", createIndex = true, dynamic = Dynamic.TRUE)
+@Data
+@Setting(shards = 1, replicas = 0)
+public class CommentModel {
+
+	@Field(type=FieldType.Integer)
+    private Integer courseID = null;
+
+    @Field(type=FieldType.Text)
+    private String content = null;
+    
+    @Field(type=FieldType.Text)
+    private String username = null;
+    
+}
