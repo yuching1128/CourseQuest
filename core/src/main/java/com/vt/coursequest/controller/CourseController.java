cf52269abb1a0cd4e19f8839348c63bc1791b0b9
swati-lamba <lswati@vt.edu>
2023-04-26 17:27:39 -0400

    adding pagenumber to the json response

--- core/src/main/java/com/vt/coursequest/controller/CourseController.java
+++ core/src/main/java/com/vt/coursequest/controller/CourseController.java
@@ -4,25 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import com.vt.coursequest.dao.ReviewRepository;
-import com.vt.coursequest.entity.Review;
-import com.vt.coursequest.entity.User;
-import com.vt.coursequest.interceptor.UserDetailsFromGoogle;
-import com.vt.coursequest.service.UserDataService;
-import lombok.extern.slf4j.Slf4j;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mysql.cj.util.StringUtils;
+import com.vt.coursequest.elasticsearch.dto.CourseListDTO;
 import com.vt.coursequest.entity.Course;
 import com.vt.coursequest.entity.Degree;
+import com.vt.coursequest.entity.Review;
+import com.vt.coursequest.entity.User;
+import com.vt.coursequest.interceptor.UserDetailsFromGoogle;
 import com.vt.coursequest.service.CourseDataService;
+import com.vt.coursequest.service.UserDataService;
 
 import io.swagger.annotations.ApiOperation;
-
-import javax.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author: EugeneFeng
@@ -45,17 +52,19 @@ public class CourseController {
 
 	@ApiOperation("This service is used to get the list of all the courses available in the university")
 	@GetMapping("/api/university/{universityId}/courses")
-	public ResponseEntity<List<Course>> getCourseList(@PathVariable String universityId,
-			@RequestParam(required = false) String pageNum,
-			@RequestParam(required = false) String pageSize) {
+	public ResponseEntity<CourseListDTO> getCourseList(@PathVariable String universityId,
+			@RequestParam(required = false) String pageNum, @RequestParam(required = false) String pageSize) {
 		List<Course> list;
+		CourseListDTO resp = null;
 		if (StringUtils.isNullOrEmpty(pageSize) && StringUtils.isNullOrEmpty(pageNum)) {
 			list = cds.findAll(Integer.parseInt(universityId));
+
+			resp = new CourseListDTO(list, list.size());
 		} else {
-			list = cds.getCourseList(Integer.parseInt(universityId), Integer.parseInt(pageNum),
+			resp = cds.getCourseList(Integer.parseInt(universityId), Integer.parseInt(pageNum),
 					Integer.parseInt(pageSize), "");
 		}
-		return list.isEmpty() ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(list, HttpStatus.OK);
+		return new ResponseEntity<>(resp, HttpStatus.OK);
 	}
 
 	@ApiOperation("This service is used to get a particular coursedetails")
@@ -78,15 +87,13 @@ public class CourseController {
 	@ApiOperation("This service is used to get the list of reviews in a specific course")
 	@GetMapping("/api/university/{universityId}/courses/{courseId}/review")
 	public ResponseEntity<List<Review>> getReviewList(@PathVariable String universityId, @PathVariable String courseId,
-			@RequestParam(required = false) String pageNum,
-			@RequestParam(required = false) String pageSize) {
+			@RequestParam(required = false) String pageNum, @RequestParam(required = false) String pageSize) {
 		List<Review> list = new ArrayList<>();
 		if (StringUtils.isNullOrEmpty(pageSize) && StringUtils.isNullOrEmpty(pageNum)) {
 			list = cds.findAllReview(Integer.parseInt(universityId), Integer.parseInt(courseId));
 		} else {
 			list = cds.getReviewList(Integer.parseInt(universityId), Integer.parseInt(courseId),
-					Integer.parseInt(pageNum),
-					Integer.parseInt(pageSize), "");
+					Integer.parseInt(pageNum), Integer.parseInt(pageSize), "");
 		}
 		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
@@ -107,11 +114,11 @@ public class CourseController {
 	@PostMapping("/api/university/{universityId}/courses/{courseId}/review")
 	public ResponseEntity<Review> addReview(@RequestBody Review review) {
 		Review reviewResponse = new Review();
-		try{
+		try {
 			UserDetailsFromGoogle userDetails = (UserDetailsFromGoogle) session.getAttribute("user_details");
 			User user = uds.findOrCreateUser(userDetails);
 			reviewResponse = cds.createReview(review, user);
-		} catch(Exception e){
+		} catch (Exception e) {
 			log.error(e.getMessage());
 		}
 		return new ResponseEntity<>(reviewResponse, HttpStatus.OK);
@@ -121,12 +128,12 @@ public class CourseController {
 	@PutMapping("/api/university/{universityId}/courses/{courseId}/review/{reviewId}")
 	public ResponseEntity<Review> updateReview(@PathVariable Integer reviewId, @RequestBody Review review) {
 		Review reviewResponse = new Review();
-		try{
+		try {
 			UserDetailsFromGoogle userDetails = (UserDetailsFromGoogle) session.getAttribute("user_details");
 			User user = uds.findOrCreateUser(userDetails);
 			review.setUser(user);
 			reviewResponse = cds.updateReview(reviewId, review);
-		}catch(Exception e){
+		} catch (Exception e) {
 			log.error(e.getMessage());
 		}
 		return new ResponseEntity<>(reviewResponse, HttpStatus.OK);
--- core/src/main/java/com/vt/coursequest/elasticsearch/dto/SearchDTO.java
+++ core/src/main/java/com/vt/coursequest/elasticsearch/dto/SearchDTO.java
@@ -5,11 +5,18 @@ import lombok.Data;
 @Data
 public class SearchDTO {
 
-	private String fullTextSearch;
+	private String fullTextSearch = "";
 
-	private String dept;
+	private String dept = "";
 
-	private String level;
+	private String level = "";
 
-	private String universityId;
+	private String universityId = "";
+
+	public boolean isEmpty() {
+		if (fullTextSearch.isBlank() && dept.isBlank() && level.isBlank() && !universityId.isBlank()) {
+			return true;
+		}
+		return false;
+	}
 }
