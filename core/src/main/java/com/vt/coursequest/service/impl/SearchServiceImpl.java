ba7e721286d3b820b21ffba2d334b6d7348fd716
swati-lamba <lswati@vt.edu>
2023-05-01 12:11:36 -0400

    elastic search issue fix

--- core/src/main/java/com/vt/coursequest/service/impl/SearchServiceImpl.java
+++ core/src/main/java/com/vt/coursequest/service/impl/SearchServiceImpl.java
@@ -2,9 +2,7 @@ package com.vt.coursequest.service.impl;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
